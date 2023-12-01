11a4cb90b26bc30f8204063ffc80852734cc9aaa
Sinovuyo Dyantyi <cnovuyo10@gmail.com>
2023-12-01 20:58:14 +0200

    added folders and files

---
+++ client/main.js
@@ -0,0 +1,48 @@
+import './style.css'
+import deliveryApp from './app'
+import './app'
+
+
+import Alpine from "alpinejs";
+import persist from '@alpinejs/persist';
+
+window.Alpine = Alpine;
+Alpine.plugin(persist);
+
+ Alpine.data('info', deliveryApp)
+
+Alpine.start();
+
+
+
+
+
+
+
+
+
+
+
+// import javascriptLogo from './javascript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.js'
+
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+//     </a>
+//     <h1>Hello Vite!</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite logo to learn more
+//     </p>
+//   </div>
+// `
+
+// setupCounter(document.querySelector('#counter'))
